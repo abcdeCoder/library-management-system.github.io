c7a9344d0d50abddd9a2d2443b56b0b1eb92c4cb
Gaurav verma <gaurav.02116403220@ipu.ac.in>
2022-07-06 17:28:23 +0530

    update2

--- card/phpqrcode/index.php
+++ card/phpqrcode/index.php
@@ -56,7 +56,6 @@
         if (trim($_REQUEST['data']) == '')
             die('data cannot be empty! <a href="?">back</a>');
             
-        $_REQUEST['data']='Name:Mohan\r\nClass:10';
         // user data
         $filename = $PNG_TEMP_DIR.'test'.md5($_REQUEST['data'].'|'.$errorCorrectionLevel.'|'.$matrixPointSize).'.png';
         QRcode::png($_REQUEST['data'], $filename, $errorCorrectionLevel, $matrixPointSize, 2);    
@@ -92,4 +91,4 @@
     // benchmark
     QRtools::timeBenchmark();    
 
-    ?>
+    
