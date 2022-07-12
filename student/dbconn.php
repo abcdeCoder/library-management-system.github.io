ab39f7d555bdc8e8e12ea13952e6dd324af74107
Gaurav verma <gaurav.02116403220@ipu.ac.in>
2022-07-12 17:40:34 +0530

    db

--- student/dbconn.php
+++ student/dbconn.php
@@ -3,8 +3,9 @@ session_start();
 $dbservername = 'sql6.freesqldatabase.com';
 $dbusername = 'sql6505353';
 $dbpassword = 'tefCRS5nNl';
+$database = 'sql6505353';
 // Create connection
-$conn = mysqli_connect($dbservername, $dbusername, $dbpassword);
+$conn = mysqli_connect($dbservername, $dbusername, $dbpassword,$database);
 // Check connection
 if (!$conn) {
     echo 'Connected unsuccessfully';
