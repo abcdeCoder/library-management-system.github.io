078d0f0d8a5e8db2c3b4e70a63110dfb94e3d01c
Gaurav verma <gaurav.02116403220@ipu.ac.in>
2022-07-12 17:49:10 +0530

    Update index.php

--- index.php
+++ index.php
@@ -10,12 +10,15 @@ require 'dbconn.php'; ?>
 <head>
 
     <title>Library Management </title>
-    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+   
 
     <!-- Meta-Tags -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="manifest" href="/site.webmanifest">
 
     <meta name="keywords"
         content="Library Member Login Form Widget Responsive, Login Form Web Template, Flat Pricing Tables, Flat Drop-Downs, Sign-Up Web Templates, Flat Web Templates, Login Sign-up Responsive Web Template, Smartphone Compatible Web Template, Free Web Designs for Nokia, Samsung, LG, Sony Ericsson, Motorola Web Design" />
