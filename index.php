b4936bc243075bb4fa69ea25a70127e6171719cf
Gaurav verma <gaurav.02116403220@ipu.ac.in>
2022-07-12 16:21:40 +0530

    Update index.php

--- index.php
+++ index.php
@@ -8,12 +8,14 @@ require 'dbconn.php'; ?>
 <!-- Head -->
 
 <head>
+<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
 
     <title>Library Management </title>
 
     <!-- Meta-Tags -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
 
     <meta name="keywords"
         content="Library Member Login Form Widget Responsive, Login Form Web Template, Flat Pricing Tables, Flat Drop-Downs, Sign-Up Web Templates, Flat Web Templates, Login Sign-up Responsive Web Template, Smartphone Compatible Web Template, Free Web Designs for Nokia, Samsung, LG, Sony Ericsson, Motorola Web Design" />
